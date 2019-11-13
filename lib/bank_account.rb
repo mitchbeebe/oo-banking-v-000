d712c700b2b6d2c37e8593bcc80c56ecce180c6f
Mitch Beebe <mitchell.beebe@flatironschool.com>
2019-11-13 21:40:51 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,9 +1,13 @@
 class BankAccount
   
   attr_reader :name
+  attr_accessor :balance, :status
 
   def initialize(name, balance = 1000, status = 'open')
     @name = name
     @balance = balance
     @status = status
+  end
+  
+  def deposit()
 end
