bb9cd7ccf0542ad167698ddfa6fa3b48f79c5238
Mitch Beebe <mitchell.beebe@flatironschool.com>
2019-11-13 22:13:45 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -24,7 +24,7 @@ class Transfer
     end
   end
   
-  def reverse_transaction
+  def reverse_transfer
     if self.status = 'complete'
       self.sender.deposit(self.amount)
       self.receiver.deposit(-self.amount)
