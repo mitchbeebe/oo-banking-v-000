171116b113b56703da4c63ea38a94a4a1d851d96
Mitch Beebe <mitchell.beebe@flatironschool.com>
2019-11-13 22:13:20 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,4 +23,9 @@ class Transfer
       "Transaction rejected. Please check your account balance."
     end
   end
+  
+  def reverse_transaction
+    if self.status = 'complete'
+      self.
+  end
 end
